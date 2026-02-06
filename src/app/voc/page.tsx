94d313f63b039cae3f9a5cedc8475915f5c4d9ca
KDT Super App <kdtsuperapp@KDTs-Mac-mini.local>
2026-02-06 12:01:20 -0500

    VOC: More margin (-180px)

--- src/app/voc/page.tsx
+++ src/app/voc/page.tsx
@@ -267,7 +267,7 @@ export default function VOCPage() {
                 className="w-full rounded-xl overflow-hidden"
                 style={{ 
                   border: '4px solid rgba(0, 255, 65, 0.5)',
-                  marginTop: '-160px',
+                  marginTop: '-180px',
                 }}
               >
                 <img 
